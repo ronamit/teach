0bd62aa367785bc86b98eb6046ee0757d0c42af8
ronamit <ronamit@campus.technion.ac.il>
2022-06-23 02:01:09 +0300

    .

---
+++ test_gpu.py
@@ -0,0 +1,8 @@
+# from tensorflow.python.client import device_lib
+# print(device_lib.list_local_devices())
+import tensorflow as tf
+import os
+
+print('LD_LIBRARY_PATH == ', os.environ.get("LD_LIBRARY_PATH"))
+
+print(tf.config.list_physical_devices('GPU'))
